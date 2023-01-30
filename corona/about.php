5b2cb51abe3b71fba81c4dba8a9c578998e07c80
samuel <damosisamuel@gmail.com>
2023-01-30 09:59:41 +0100

    about.php

---
+++ corona/about.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About</title>
+    <link rel="stylesheet" href="style.css">
+    <!-- link below cdn and bootrap link -->
+    <link rel="stylesheet" href="cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css"
+        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.11.5/datatables.min.css" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.11.5/datatables.min.js"></script>
+    <!-- bootsrap link -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles.css">
+    <!-- cdn -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
+        integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+
+<header class="myheader">
+     <?php include('header.php'); ?>
+
+
+    </header>   
+    <main class="">
+           
+           <div class=" row mt-5">
+               <div class="goli offset-1 col-10 mt-3 pt-2 ">
+                  <div class="row">
+                      <div class="col-6 pt-4">
+                          <div class="mt-5">
+                              <h1>ABOUT CORONA VIRUS</h1>
+                              <p class="myh"></p>
+                              <p>English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for</p>
+                              
+                              <button class="readmore">ABOUT MORE</button>
+                          </div>
+
+                      </div>
+                      <div class="col-4 offset-2">
+                          <img class="coror" src="cor.png" alt="">
+
+                      </div>
+
+                  </div>
+
+               </div>
+
+           </div>
+    
+
+       </section>
+
+    </main>
+    <footer>
+        <?php include('footer.php');?>
+
+    </footer>
+</body>
+</html>
